f22857a816e058ed2a5901c956f20708654542dc
Helin Hümeyra Saraçoğlu <helinhumeyrasaracoglu@gmail.com>
2025-09-04 16:04:11 +0300

    [SP-3] - @Transactional Kullanımı

--- src/main/java/com/helin/springboot/service/impl/UserServiceImpl.java
+++ src/main/java/com/helin/springboot/service/impl/UserServiceImpl.java
@@ -3,20 +3,21 @@ package com.helin.springboot.service.impl;
 import com.helin.springboot.entity.User;
 import com.helin.springboot.repository.UserRepository;
 import com.helin.springboot.service.UserService;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
     @Override
+    @Transactional
     public User createUser(User user) {
         if (userRepository.existsByUsername(user.getUsername())) {
             throw new IllegalArgumentException("Dikkat! Kullanıcı adı zaten kayıtlı");
@@ -26,19 +27,18 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    @Transactional()
     public User readUser(Long id){
         return userRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Bu id ile eşleşen Kullanıcı bulunamadı." + id));
     }
 
     @Override
-    @Transactional()
     public List<User> findAll() {
         return userRepository.findAll();
     }
 
     @Override
+    @Transactional
     public User updateUser(Long id, User updated) {
         User user = readUser(id);
         if (updated.getUsername().equals(user.getUsername())  && userRepository.existsByUsername(updated.getUsername())) {
@@ -48,6 +48,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
     }
